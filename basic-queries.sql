3a95b70de72abd76e7e16cb3ff31b68e2b0d5702
10197jsg <10197jsg@gmail.com>
2025-04-10 12:43:47 +0200

    Solved lab

--- basic-queries.sql
+++ basic-queries.sql
@@ -1,30 +1,43 @@
-/* 1. Display all available tables in the Sakila database.
-*/
+/* 1. Display all available tables in the Sakila database. */
 
+SHOW Tables;
 
-/* 2. Retrieve all the data from the tables `actor`, `film` and `customer`.
-*/
+/* 2. Retrieve all the data from the tables `actor`, `film` and `customer`.*/
 
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
 
 /* 3. Retrieve the following columns from their respective tables:
 	- 3.1 Titles of all films from the `film` table
 	- 3.2 List of languages used in films, with the column aliased as `language` from the `language` table
 	- 3.3 List of first names of all employees from the `staff` table*/
 
-/* 4. Retrieve unique release years.
-*/
+SELECT title FROM film;
+SELECT name FROM language;
+SELECT first_name FROM staff;
 
+/* 4. Retrieve unique release years.*/
+
+SELECT DISTINCT release_year FROM film;
 
 /* 5. Counting records for database insights:
 	- 5.1 Determine the number of stores that the company has.
 	- 5.2 Determine the number of employees that the company has.
 	- 5.3 Determine how many films are available for rent and how many have been rented.
-	- 5.4 Determine the number of distinct last names of the actors in the database.
-*/
+	- 5.4 Determine the number of distinct last names of the actors in the database.*/
+
+SELECT COUNT(store_id) AS stores_number FROM store;
+SELECT COUNT(staff_id) AS staff_number FROM staff;
+SELECT COUNT(rental_id) AS films_rented FROM rental;
+SELECT COUNT(DISTINCT inventory_id) AS films_rented FROM rental;
     
-    
-/* 6. Retrieve the 10 longest films.
-*/
+/* 6. Retrieve the 10 longest films.*/
+
+SELECT title, length
+FROM film
+ORDER BY length DESC
+LIMIT 10;
 
 
 /*7. Use filtering techniques in order to:
@@ -34,3 +47,7 @@
 		- *Hint: use `LIKE` operator. [More information here.](https://www.w3schools.com/sql/sql_like.asp)*
 	- 7.3 Determine the number of films that include Behind the Scenes content
     */
+
+SELECT *
+FROM actor
+WHERE first_name = 'SCARLETT';
